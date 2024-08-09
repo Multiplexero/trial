4d29ec9afd958c256388d2c94c5554c1025aafa2
trilok2003 <trilokthakur1212@gmail.com>
2024-08-10 02:12:51 +0530

    Deployment2

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ export const Register = async (req, res) => {
 }
 export const Login = async (req, res) => {
     try {
+        console.log("hii login");
         const { email, password } = req.body;
         if (!email || !password) {
             return res.status(401).json({
